e80dfbfbc86b201243320d5e5f58aebf5f86ea75
Tim Seufert <116903809+Timhongphuc@users.noreply.github.com>
2026-04-07 23:25:12 +0100

    Minor improvements (WIP)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 @app.get("/")
 def hello():
-    return {"headline": "Hello, dear developer!", "text_info": "I'm Tim Seufert, a 16 y.o. high school developer and hobbyist with a strong focus on developing well designed, user friendly AI solutions. I'm a student working on personal projects to enhance my software development skills. I've built the FakifyAPI a few weeks after finalizing my initial Fakify (fake news detector) project. I wanted to create a way for users of the internet to analyze and validate information from sources like articles or blog posts in a secure and reliable way (through retrieval augmented generation with the Mistral and Exa API's). Today I'am proudly presenting the FakifyAPI. This interface should allow users to embed my Fakify service into their own applications and services. All in all my project aims for an easy use of the Fakify project and making the internet a little bit safer. I hope you will enjoy using this API. See you on the internet :) - Tim Seufert", "api_status": "API is operational", "documentation": "/docs", "about_the_project": "https:github.com/Timhongphuc/FakifyAPI"}
+    return {"headline": "Hello, dear developer!", "text_info": "I'm Tim Seufert, a 16 y.o. high school developer and hobbyist with a strong focus on developing well designed, user friendly AI solutions. I'm a student working on personal projects to enhance my software development skills. I've built the FakifyAPI a few weeks after finalizing my initial Fakify (fake news detector) project. I wanted to create a way for users of the internet to analyze and validate information from sources like articles or blog posts in a secure and reliable way (through retrieval augmented generation with the Mistral and Exa API's). Today I'am proudly presenting the FakifyAPI. This interface should allow users to embed my Fakify service into their own applications and services. All in all my project aims for an easy use of the Fakify project and making the internet a little bit safer. I hope you will enjoy using this API. See you on the internet :) - Tim Seufert", "api_status": "API is operational", "documentation": "/docs and https://", "about_the_project": "https:github.com/Timhongphuc/FakifyAPI"}
 
 @app.get("/app")
 def analysis(url: str):
@@ -117,7 +117,8 @@ def analysis(url: str):
 
     return {"results": f"{results_final}"}
 
-@app.get("/")
-def
+@app.get("/stats")
+def stats():
+    return{"Hello": "Hello World!"}
 
 uvicorn.run(app)
